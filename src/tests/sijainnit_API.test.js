248973b0a9e4055109e864defff9b4e81856d6ea
Muath Othman <muath.othman@gmail.com>
2024-02-20 18:23:41 +0200

    Changed test command to jest tests --forceExit

--- src/tests/sijainnit_API.test.js
+++ src/tests/sijainnit_API.test.js
@@ -2,6 +2,7 @@ const supertest = require('supertest');
 const app = require('../app.js');
 const request = supertest(app);
 
+
 // Test the GET route
 test('gets the sijainnit endpoint', async () => {
     const response = await request.get('/sijainnit');
