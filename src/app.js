f4dd625fc2e42735a3a4f1605bf305eafe6e5782
Muath Othman <muath.othman@gmail.com>
2024-02-20 16:05:46 +0200

    Added gets the sijainnit test

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const sijainnitRoutes = require('./routes/sijainnitRoutes');
 const otpRoutes = require('./routes/otpRoutes');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 
 app.use(express.json());
@@ -14,6 +14,7 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
+module.exports = app;
 
 //////
 
---
+++ src/tests/sijainnit_API.test.js
@@ -0,0 +1,9 @@
+const supertest = require('supertest');
+const app = require('../app.js');
+const request = supertest(app);
+
+// Test the GET route
+test('gets the sijainnit endpoint', async () => {
+    const response = await request.get('/sijainnit');
+    expect(response.status).toBe(200);
+});
