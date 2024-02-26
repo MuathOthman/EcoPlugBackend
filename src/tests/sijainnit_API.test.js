b6c5fb2cdf5cbbeaf47aec1c346fb74a7199fcb6
Muath Othman <muath.othman@gmail.com>
2024-02-26 18:52:51 +0200

    Added new test cases

--- src/tests/sijainnit_API.test.js
+++ src/tests/sijainnit_API.test.js
@@ -13,5 +13,32 @@ test('gets the sijainnit endpoint', async () => {
 
 });
 
+test('gets free parking spots', async () => {
+    const response = await request.get('/sijainnit/specific/10');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({
+                count: expect.any(Number)
+            })
+        ])
+    );
+}
+);
+
+test('gets parking spots', async () => {
+    const response = await request.get('/sijainnit/parkings/2');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({
+                parkki: expect.any(String),
+                tila: expect.any(Number)
+            })
+        ])
+    );
+}
+);
+
 
 
