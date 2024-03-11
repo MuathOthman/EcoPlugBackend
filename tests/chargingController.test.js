9ed3e96bd3a9b8c96a90e3d4bdbc3955e9258921
Muath Othman <muath.othman@gmail.com>
2024-03-11 15:12:43 +0200

    Added charging tests

--- tests/chargingController.test.js
+++ tests/chargingController.test.js
@@ -2,11 +2,16 @@ const supertest = require('supertest');
 const app = require('../app');
 const request = supertest(app);
 
+let randomPisteID = Math.floor(Math.random() * 141);
+let randomLatausID = Math.floor(Math.random() * 201);
+
+
+
 describe('Charging Controller Tests', () => {
     test('should start charging', async () => {
         const response = await request.post('/charging/start-charging').send({
-            latauspisteID: 3,
-            latausID: 81,
+            latauspisteID: randomPisteID,
+            latausID: randomLatausID,
         });
         expect(response.status).toBe(201);
         expect(response.body).toEqual({ message: 'Charging stated' });
